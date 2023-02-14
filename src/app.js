ff62b8e085689e76a6099cf1ebc15fa58354fbaa
Ivo Dimitrov <idimitrov@easypay.bg>
2023-02-14 22:02:45 +0200

    break it

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 const dayOfTheWeek = (date = new Date()) => {
-    const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thrsday', 'Friday', 'Saturday'];
+    const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thuuursday', 'Friday', 'Saturday'];
 
     return days[date.getDay()];
 }
